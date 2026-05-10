127a9134efba02fa6d56b2d5f986c12fa59c1e15
themedworld <99541159+themedworld@users.noreply.github.com>
2026-05-10 13:53:28 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,100 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pandas as pd
+import numpy as np
+import joblib
+
+# Charger le modèle
+pipeline = joblib.load("best_model_pipeline.joblib")
+
+app = FastAPI(
+    title="Call Center Cost Prediction API",
+    version="1.0"
+)
+
+# Colonnes attendues
+EXPECTED_COLS = [
+    '0','1','2','3','4','5','8','9',
+    '6__Sales','6__Support','6__Tech','6__callTypes',
+    '7__AI','7__CRM','7__CTI','7__IVR',
+    '7__Training','7__WFM','7__dependencies'
+]
+
+# ----------------------------
+# Request Body
+# ----------------------------
+class PredictionRequest(BaseModel):
+    numberOfAgents: int
+    numberOfCallsPerDay: int
+    averageHandleTimeSec: float
+    estimatedDurationDays: int
+    slaTargetSeconds: int
+    risksScore: float
+    callTypes: str
+    dependencies: str
+    CSAT: float
+    FCR: float
+
+# ----------------------------
+# Multi-label Expansion
+# ----------------------------
+def expand_project(data, sep=';'):
+
+    calltypes = str(data.callTypes).split(sep)
+    calltypes = [x.strip() for x in calltypes]
+
+    deps = str(data.dependencies).split(sep)
+    deps = [x.strip() for x in deps]
+
+    row = {
+        '0': data.numberOfAgents,
+        '1': data.numberOfCallsPerDay,
+        '2': data.averageHandleTimeSec,
+        '3': data.estimatedDurationDays,
+        '4': data.slaTargetSeconds,
+        '5': data.risksScore,
+        '8': data.CSAT,
+        '9': data.FCR,
+    }
+
+    # callTypes
+    for lab in ['6__Sales','6__Support','6__Tech','6__callTypes']:
+        label = lab.split('__')[1]
+        row[lab] = 1 if label in calltypes else 0
+
+    # dependencies
+    for lab in [
+        '7__AI','7__CRM','7__CTI',
+        '7__IVR','7__Training',
+        '7__WFM','7__dependencies'
+    ]:
+        label = lab.split('__')[1]
+        row[lab] = 1 if label in deps else 0
+
+    return row
+
+# ----------------------------
+# Routes
+# ----------------------------
+
+@app.get("/")
+def home():
+    return {
+        "message": "ML API is running"
+    }
+
+@app.post("/predict")
+def predict(data: PredictionRequest):
+
+    row = expand_project(data)
+
+    df = pd.DataFrame([row], columns=EXPECTED_COLS)
+
+    for c in df.columns:
+        df[c] = pd.to_numeric(df[c], errors='coerce')
+
+    prediction = pipeline.predict(df)[0]
+
+    return {
+        "predicted_budget_tnd": round(float(prediction), 2)
+    }
